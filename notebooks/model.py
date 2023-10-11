b7b0642618079f0c6f3c30ce86f02f0d12ca6f94
Henry Williams <henrywilliams95000@gmail.com>
2023-10-11 12:44:34 +0100

    started on interim report

---
+++ notebooks/model.py
@@ -0,0 +1,43 @@
+import torch 
+import torchvision.models as models 
+import torch.nn as nn
+import torch.nn.functional as F 
+import math 
+
+class ImageModel(nn.Module):
+    def __init__(self):
+        super(ImageModel, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.CrossMapLRN2d(size=5, alpha=0.0001, beta=0.75, k=1.0),
+            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2, groups=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.CrossMapLRN2d(size=5, alpha=0.0001, beta=0.75, k=1.0),
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 64, kernel_size=1, stride=1, padding=0),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+
+class FaceModel(nn.Module):
+    def __init__(self):
+        super(FaceModel, self).__init__()
+        self.conv = ImageModel()
+        self.fc = nn.Sequential(
+            nn.Linear(9,216, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, 64),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.fc(x)
+        return x 
